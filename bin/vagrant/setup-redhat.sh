49826eb7ce3363cded16f1c82d2cb2ede4d9d28c
Alessandro Franceschi <al@example42.com>
2015-08-29 22:37:37 +0200

    Fixed setup script for Puppet 3 on Centos 7

--- bin/vagrant/setup-redhat.sh
+++ bin/vagrant/setup-redhat.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd /root
 
-case "$1" in
-  original) puppetsuffix='' ;;
-  latest) puppetsuffix='' ;;
-  *) puppetsuffix="-${1}.el6" ;;
-esac
-
 echo "## Installing latest Puppet version and dependencies"
 
 rpm -qi epel-release >/dev/null
@@ -14,19 +8,13 @@ if [ "x$?" == "x1" ] ; then
   rpm -ivh http://ftp.colocall.net/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm >/dev/null # 2>&1
 fi
 
-if [ "x$1" != "xoriginal" ] ; then
-  rpm -qa | grep puppetlabs-release >/dev/null # 2>&1
-  if [ "x$?" == "x1" ] ; then
-    rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-11.noarch.rpm >/dev/null # 2>&1
-  fi
-fi
-
-rpm -qi puppet$puppetsuffix >/dev/null 2>&1
+rpm -qa | grep puppetlabs-release >/dev/null # 2>&1
 if [ "x$?" == "x1" ] ; then
-  yum install -y puppet$puppetsuffix >/dev/null # 2>&1
+  rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-11.noarch.rpm >/dev/null # 2>&1
 fi
 
-my_exit = $?
-puppet --version
+rpm -qi puppet >/dev/null 2>&1
+if [ "x$?" == "x1" ] ; then
+  yum install -y puppet >/dev/null # 2>&1
+fi
 
-exit $my_exit
