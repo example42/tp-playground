71f5249ec8c0e5f6a415f36734f79f553e51299d
Alessandro Franceschi <al@example42.com>
2015-11-08 07:23:45 +0100

    Refactored test.sh script

--- bin/test.sh
+++ bin/test.sh
@@ -9,10 +9,13 @@ uninstall_whitelist='ssh|vim|lsb|puppet|apt'
 show_help () {
   echo
   echo "You must specify the application you want to test and the VM to use:"
-  echo "$0 <application> <vm> [acceptance]"
+  echo "$0 <application> <vm> [check_mode]"
   echo
   echo "To test redis on Ubuntu1404:"
   echo "$0 redis Ubuntu1404"
+  echo
+  echo "To test redis on all running VMs:"
+  echo "$0 redis all"
   echo
   echo "To test all apps on Ubuntu1404:"
   echo "$0 all Ubuntu1404 "
@@ -22,6 +25,9 @@ show_help () {
   echo
   echo "To run puppi check on Ubuntu1404 and puppi checks results:"
   echo "$0 redis Ubuntu1404 puppi"
+  echo
+  echo "To test all apps on all running VMs:"
+  echo "$0 all all "
 }
 
 if [ "x$2" == "x" ]; then
@@ -31,46 +37,57 @@ fi
 
 app=$1
 vm=$2
-mode=$3
+if [ "x$3" == "x" ]; then
+  mode='default'
+else
+  mode=$3
+fi
 case $mode in 
     acceptance) mode_param="test_enable => true," ;;
     puppi) mode_param="puppi_enable => true," ;;
 esac
 
-# Workaround to use Ruby 193 on Centos6
-if [ "x$vm" == "xCentos65" ]; then
-  envs="env PATH=/opt/puppetlabs/bin:/opt/rh/ruby193/root/usr/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin LD_LIBRARY_PATH=/opt/rh/ruby193/root/usr/lib64"
-else
-  envs='env PATH=/opt/puppetlabs/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/vagrant/bin'
-fi
-
 options="$PUPPET_OPTIONS --verbose --report --show_diff --pluginsync --summarize --modulepath '/vagrant/modules_local:/vagrant/modules:/etc/puppet/modules' "
 command="sudo -i $envs puppet apply"
 
-acceptance_test () {
+install() {
+  i_app=$1
+  i_vm=$2  
+  echo_title "Installing $i_app on $i_vm"
+  vagrant ssh $i_vm -c "$command $options -e 'tp::install { $i_app: $mode_param }'"
+}
+
+uninstall() {
+  u_app=1
+  u_vm=$2  
+  if [[ "$u_app" =~ $uninstall_whitelist ]]; then
+    echo_title "Skipping Uninstallation of $u_app on $u_vm"
+  else
+    echo_title "Uninstalling $u_app on $u_vm"
+    vagrant ssh $u_vm -c "which apt >/dev/null 2>&1 || apt-get -f install"
+    vagrant ssh $u_vm -c "$command $options -e 'tp::uninstall { $u_app: }'"
+  fi
+}
+
+default_check() {
+  echo 
+}
+acceptance_check () {
   echo_title "Running acceptance test for $1 on $2"
   rm -f acceptance/$2/success/$1
   rm -f acceptance/$2/failure/$1
   vagrant ssh $2 -c "sudo /etc/tp/test/$1" > /tmp/tp_test_$1_$2
   if [ "x$?" == "x0" ]; then
-    mkdir -p acceptance/$2/success
-    mv /tmp/tp_test_$1_$2 acceptance/$2/success/$1
-    cat acceptance/$2/success/$1
-    echo_success "SUCCESS! Output written to acceptance/$2/success/$1"
+    result='success'
   else
-    mkdir -p acceptance/$2/failure
-    mv /tmp/tp_test_$1_$2 acceptance/$2/failure/$1
-    cat acceptance/$2/failure/$1
-    echo_failure "FAILURE! Output written to acceptance/$2/failure/$1"
+    result='failure'
   fi
+  mkdir -p acceptance/$2/$result
+  mv /tmp/tp_test_$1_$2 acceptance/$2/$result/$1
+  cat acceptance/$2/$result/$1
+  echo_$result "## ${result}! ## Output written to acceptance/$2/${result}/$1"
 
-  if [[ "$1" =~ $uninstall_whitelist ]]; then
-    echo_title "Skipping Uninstallation of $1 on $2"
-  else
-    echo_title "Uninstalling $1 on $2"
-    vagrant ssh $2 -c "which apt >/dev/null 2>&1 || apt-get -f install"
-    vagrant ssh $2 -c "$command $options -e 'tp::uninstall { $1: }'"
-  fi
+  uninstall $1 $2
 }
 
 puppi_check () {
@@ -86,34 +103,23 @@ puppi_check () {
 
 if [ "x${app}" == "xall" ]; then
   for a in $(ls -1 modules/tinydata/data | grep -v default.yaml | grep -v test) ; do
-    echo_title "Installing $a on $vm"
-    vagrant ssh $vm -c "$command $options -e 'tp::install { $a: $mode_param }'"
-    if [ "x${mode}" == "xacceptance" ]; then
-      acceptance_test $a $vm
-    fi
-    if [ "x${mode}" == "xpuppi" ]; then
-      puppi_check $a $vm
+    if [ "x${vm}" == "xall" ]; then
+      for v in $(vagrant status | grep running | cut -d ' ' -f1) ; do
+        install $a $v
+        ${mode}_check $a $v
+      done
+    else
+      install $a $vm
+      ${mode}_check $a $vm
     fi
   done
 elif [ "x${vm}" == "xall" ]; then
-  for a in $(vagrant status | grep running | cut -d ' ' -f1) ; do
-    echo_title "Installing $app on $a"
-    vagrant ssh $a -c "$command $options -e 'tp::install { $app: $mode_param }'"
-    if [ "x${mode}" == "xacceptance" ]; then
-      acceptance_test $app $a
-    fi
-    if [ "x${mode}" == "xpuppi" ]; then
-      puppi_check $app $a
-    fi
+  for v in $(vagrant status | grep running | cut -d ' ' -f1) ; do
+    install $app $v
+    ${mode}_check $app $v
   done
 else
-  echo_title "Installing $app on $vm"
-  vagrant ssh $vm -c "$command $options -e 'tp::install { $app: $mode_param }'" 
-  if [ "x${mode}" == "xacceptance" ]; then
-    acceptance_test $app $vm
-  fi
-  if [ "x${mode}" == "xpuppi" ]; then
-    puppi_check $app $vm
-  fi
+  install $app $vm
+  ${mode}_check $app $vm
 fi
 
